c910ae12d6960c03651969b5cf796ad0aedb40f5
mwerumuchai <mwerumuchai@gmail.com>
2017-08-11 04:15:58 +0300

    Add form

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var addressInput = $("input#address").val();
+    var emailInput= $("input#email").val();
+    var phonenoInput = $("input#phoneno").val();
 
+    $(".person1").text(person1Input);
+    $(".address").text(addressInput);
+    $(".email").text(emailInput);
+    $(".phoneno").text(phonenoInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
